f98e5423a683e26c5c184b1829d41683a2dc0741
montexbjeliseo <secondmtx@gmail.com>
2023-12-31 15:01:06 -0300

    BackToTop

--- src/shared/components/Layout/index.tsx
+++ src/shared/components/Layout/index.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "../Navbar"
 import { Footer } from "../Footer"
+import { TopButton } from "../TopButton"
 
 export const Layout = () => {
     return (
         <>
             <Navbar />
-
             <Outlet />
-
+            <TopButton />
             <Footer />
         </>
     )
---
+++ src/shared/components/TopButton/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { ArrowUpIcon } from "../../Icons";
+
+const TopButtonStyled = styled.button`
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 32px;
+    height: 32px;
+
+    &.visible {
+        display: block;
+    }
+`;
+
+export const TopButton = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > window.innerHeight) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <TopButtonStyled className={`scroll-btn ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
+      <ArrowUpIcon />
+    </TopButtonStyled>
+  );
+}
